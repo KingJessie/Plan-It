850e5b2764336ab5a4af662699208aab6129cdb9
kingjessie <jessie.kinganga@gmail.com>
2023-01-25 16:03:09 +0000

    work day scheduler js file complete

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
+// Display current date and time
 
-  var currentDay = moment().format("dddd D MMMM YYYY, h:mm:ss a");
-  $("#currentDay").text(currentDay);
+var currentDay = moment().format("dddd D MMMM YYYY, h:mm:ss a");
+$("#currentDay").text(currentDay);
 
-  
+
+// Variable to store the saved button element, which will be used as an event listener when clicked.
+var saveBtn = $(".saveBtn button");
+
+
+// Save text to local storage when button is clicked
+
+saveBtn.on("click", function () {
+  var hour = $(this).attr("class");
+  var savedText = $(this).parent().siblings(".textArea").val();
+
+  localStorage.setItem(hour, savedText);
+});
+
+
+
+// Function for time block to change color when hour is in present, past or future
+
+function colorChange() {
+  var hour = moment().hours();
+
+  $(".time-block").each(function () {
+    var currentHour = parseInt($(this).attr("id"));
+
+    if (currentHour > hour) {
+      $(this).addClass("future");
+    } else if (currentHour === hour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("past");
+    }
+  });
+}
+
+colorChange();
+
+
+
